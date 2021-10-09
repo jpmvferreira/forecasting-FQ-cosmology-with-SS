781745b64c61224111b9c0a0ad84b05d98d7f76d
JPUnD <jose@jpferreira.me>
2021-10-09 23:17:05 +0100

    added chain coloring on time series

--- run.py
+++ run.py
@@ -12,6 +12,11 @@ import yaml
 
 # main
 def main():
+    # user defined parameters
+    chains = 2
+    warmup = 1000
+    samples = 10000
+
     # get relevant parameters from .yml file
     with open("model/gaussian.yml", "r") as file:
         d = yaml.full_load(file)
@@ -20,47 +25,44 @@ def main():
         labels = d["labels"]
         initial = d["initial"]
 
-    # get model from the .stan file
-    with open("model/gaussian.stan", "r") as file:
-        program = file.read()
-
-    # user defined parameters
-    chains = 2
-    warmup = 20
-    samples = 1000
-
-    # get data from .csv file
-    csv = pandas.read_csv("data/gaussian.csv", comment="#")
-    values = list(csv["value"])
-    data = {"n": len(values), "y": values}
-
-    # get initial conditions
+    # get initial conditions specified in the .yml file
     init = []
     for i in range(0, chains):
         init.append({})
         for name in names:
             init[i][name] = eval(initial[name])
 
-    print(init)
+    # get model from the .stan file
+    with open("model/gaussian.stan", "r") as file:
+        program = file.read()
+
+    # get data from .csv file
+    csv = pandas.read_csv("data/gaussian.csv", comment="#")
+    values = list(csv["value"])
+    data = {"n": len(values), "y": values}
+
     # run the sampler
     posterior = stan.build(program, data=data)
-    fit = posterior.sample(num_chains=chains, num_samples=samples, num_warmup=warmup, init=init)
+    fit = posterior.sample(num_chains=chains, num_samples=samples, num_warmup=warmup, init=init, save_warmup=True)
 
-    # plot the time series for each parameter
+    # plot the time series
     fig, axes = plt.subplots(ndim, figsize=(10, 7), sharex=True)
+    steps = np.arange(samples+warmup)
     for i in range(ndim):
         ax = axes[i]
-        ax.plot(fit[names[i]][0], "k", alpha=0.75)
+        for j in range(0, chains):
+            ax.plot(steps, fit[names[i]][0][j::chains], alpha=0.75)
         ax.set_xlim(0, samples)
         ax.set_ylabel(labels[i])
-        #ax.axvline(x=warmup, linestyle="--", color="red")
+        ax.axvline(x=warmup, linestyle="--", color="red")
         ax.yaxis.set_label_coords(-0.1, 0.5)
         ax.grid()
     axes[-1].set_xlabel("step number")
     plt.show()
 
-    # plot the corner plot
-    samples = np.column_stack((fit["mu"][0], fit["sigma"][0]))
+    # show the corner plot
+    print(len(fit["sigma"][0][chains*warmup:]))
+    samples = np.column_stack((fit["mu"][0][chains*warmup:], fit["sigma"][0][chains*warmup:]))
     mcsamples = MCSamples(samples=samples, names = ["mu", "sigma"], labels = ["\mu", "\sigma"])
     g = plots.get_subplot_plotter()
     g.triangle_plot(mcsamples, filled=True, markers={"mu": 2, "sigma": 3})

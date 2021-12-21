36f01af767de6f96ad1596980998ff5989bd255f
JPUnD <jose@jpferreira.me>
2021-12-21 13:39:17 +0000

    added custom cosmology python scripts

---
+++ cosmology/belgacem.py
@@ -0,0 +1,28 @@
+## belgacem.py
+# cosmological model from 1805.0873: ΛCDM with Ωm = 0.3087 and H0 = 67.64 km s⁻¹ Mpc⁻¹
+
+
+# imports
+from scipy.integrate import quad
+
+
+# Hubble function
+def H(z):
+    # Hubble constant
+    H0 = 70
+    H0 = H0*3.240779289*10**(-20)
+
+    # value for Ωm
+    Ωm = 0.3087
+
+    return H0*(Ωm*(1+z)**3 + 1-Ωm)**0.5
+
+
+# luminosity distance
+def dL(z, H):
+    c = 9.715611890800001e-18  # speed of light [Gpc/s]
+    return (1+z) * c * quad(lambda Z: 1/H(Z), 0, z)[0]
+
+
+# description
+description = "ΛCDM (Ωm = 0.3087, H0 = 67.64 km s⁻¹ Mpc⁻¹)"
---
+++ cosmology/very-high.py
@@ -0,0 +1,28 @@
+## very-high.py
+# cosmological model with very high parameter values: ΛCDM with Ωm = 0.450 and h = 0.9
+
+
+# imports
+from scipy.integrate import quad
+
+
+# Hubble function
+def H(z):
+    # using c/H0 = (2.9979 Gpc)/h and providing the value for h
+    h = 0.9
+    H0 = 299792458*h/(2.9979*3.085678*10**25)
+
+    # value for Ωm
+    Ωm = 0.450
+
+    return H0*(Ωm*(1+z)**3 + 1-Ωm)**0.5
+
+
+# luminosity distance
+def dL(z, H):
+    c = 9.715611890800001e-18  # speed of light [Gpc/s]
+    return (1+z) * c * quad(lambda Z: 1/H(Z), 0, z)[0]
+
+
+# description
+description = "ΛCDM (Ωm = 0.450, h = 0.9)"
